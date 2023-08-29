6ee4eb2998154016d06279de66a801ac8f298aca
Mohamedesmat1432 <Mohamedesmat1432@github.com>
2023-08-29 15:11:03 +0300

    Revert "Revert "create it licenses project""
    This reverts commit cc4c90b399f22462e1fe0851c59661cdaa07ddab.

---
+++ app/Http/Livewire/User/UserComponent.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use App\Models\Department;
+use App\Models\User;
+use App\Traits\UserTrait;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Component;
+
+class UserComponent extends Component
+{
+    use UserTrait;
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'sortBy' => ['except' => 'id'],
+        'sortAsc' => ['except' => true]
+    ];
+
+    public function mount(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function render()
+    {
+        $users = $this->user->when($this->search, function ($query) {
+            return $query->where(function ($query) {
+                $query->where('name', 'like', '%' . $this->search . '%')
+                    ->orWhere('email', 'like', '%' . $this->search . '%');
+            });
+        })->orderBy($this->sortBy, $this->sortAsc ? 'ASC' : 'DESC')->paginate(10);
+
+        $departments = Department::select('id', 'name')->get();
+
+        return view('livewire.user.user-component', [
+            'users' => $users,
+            'departments' => $departments
+        ]);
+    }
+
+    public function confirmUserAdd()
+    {
+        $this->resetItems();
+        $this->confirmForm = true;
+    }
+
+    public function saveUser()
+    {
+        $validated = $this->validate();
+        $validated['password'] = Hash::make($this->password);
+        $this->user->create($validated);
+        $this->createMessage('User');
+        $this->confirmForm = false;
+    }
+
+    public function confirmUserEdit($id)
+    {
+        $this->resetItems();
+        $this->confirmForm = true;
+        $user = $this->user->findOrFail($id);
+        $this->userId = $user->id;
+        $this->name = $user->name;
+        $this->email = $user->email;
+        $this->department_id = $user->department_id;
+        $this->role = $user->role;
+    }
+
+    public function updateUser()
+    {
+        $validated = $this->validate();
+        $user = $this->user->findOrFail($this->userId);
+        $user->update($validated);
+        $this->updateMessage('User');
+        $this->confirmForm = false;
+    }
+
+    public function confirmUserDeletion($id)
+    {
+        $this->confirmDeletion = true;
+        $this->userId = $id;
+    }
+
+    public function deleteUser()
+    {
+        $user = $this->user->findOrFail($this->userId);
+        $user->delete();
+        $this->deleteMessage('User');
+        $this->confirmDeletion = false;
+    }
+}
